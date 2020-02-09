c766466f4a9786b19c8887a0d6cd5f4adbfe9b73
Aydın Ayaydın <aydnayaydn@hotmail.com>
2020-02-09 22:41:15 +0300

    first commit

---
+++ ProjectRenamer/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ProjectRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdSlnFiles = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblFileLocation = new System.Windows.Forms.Label();
+            this.txtFindKey = new System.Windows.Forms.TextBox();
+            this.txtReplaceKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSelectFile);
+            this.groupBox1.Controls.Add(this.lblFileLocation);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 62);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select .sln File";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFile.Location = new System.Drawing.Point(247, 11);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Browse";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblFileLocation
+            // 
+            this.lblFileLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFileLocation.Location = new System.Drawing.Point(6, 16);
+            this.lblFileLocation.Name = "lblFileLocation";
+            this.lblFileLocation.Size = new System.Drawing.Size(218, 33);
+            this.lblFileLocation.TabIndex = 0;
+            // 
+            // txtFindKey
+            // 
+            this.txtFindKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFindKey.Location = new System.Drawing.Point(21, 104);
+            this.txtFindKey.Name = "txtFindKey";
+            this.txtFindKey.Size = new System.Drawing.Size(313, 20);
+            this.txtFindKey.TabIndex = 1;
+            // 
+            // txtReplaceKey
+            // 
+            this.txtReplaceKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReplaceKey.Location = new System.Drawing.Point(21, 155);
+            this.txtReplaceKey.Name = "txtReplaceKey";
+            this.txtReplaceKey.Size = new System.Drawing.Size(313, 20);
+            this.txtReplaceKey.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Find What : ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Replace With : ";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReplace.Enabled = false;
+            this.btnReplace.Location = new System.Drawing.Point(140, 192);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 5;
+            this.btnReplace.Text = "Replace All";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMessage.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblMessage.Location = new System.Drawing.Point(21, 232);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(313, 23);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 273);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReplaceKey);
+            this.Controls.Add(this.txtFindKey);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdSlnFiles;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblFileLocation;
+        private System.Windows.Forms.TextBox txtFindKey;
+        private System.Windows.Forms.TextBox txtReplaceKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ ProjectRenamer/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string _folderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            ofdSlnFiles.Filter = @"Sln Files (*.sln) | *.sln;";
+            var dialogResult = ofdSlnFiles.ShowDialog();
+
+            if (dialogResult != DialogResult.OK)
+                return;
+
+            var fileName = ofdSlnFiles.FileNames[0];
+            var indexer = fileName.LastIndexOf("\\", StringComparison.Ordinal);
+            _folderPath = fileName.Substring(0, indexer);
+            lblFileLocation.Text = _folderPath;
+
+            btnReplace.Enabled = true;
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directory = new DirectoryInfo(_folderPath);
+            try
+            {
+                ChangeFileName(directory, txtFindKey.Text, txtReplaceKey.Text);
+                lblMessage.ForeColor = Color.Green;
+                lblMessage.Text = @"Done";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.ForeColor = Color.DarkRed;
+                lblMessage.Text = @"Fail : " + ex.Message;
+            }
+        }
+
+        private void ChangeFileName(DirectoryInfo directory, string currentName, string replacementName)
+        {
+            if (directory.FullName != directory.FullName.Replace(currentName, replacementName))
+            {
+                Directory.Move(directory.FullName, directory.FullName.Replace(currentName, replacementName));
+                directory = new DirectoryInfo(directory.FullName.Replace(currentName, replacementName));
+            }
+
+            if (directory.Name == "bin" || directory.Name == "obj")
+            {
+                DeleteDirectory(directory.FullName);
+                return;
+            }
+                
+
+
+            FileInfo[] files = directory.GetFiles();
+
+            ChangeFileNameSpace(files, currentName, replacementName);
+
+
+            var directories = directory.GetDirectories();
+
+            if (directories.Length == 0) return;
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                ChangeFileName(directories[i], currentName, replacementName);
+            }
+        }
+
+        private void ChangeFileNameSpace(FileInfo[] files, string currentName, string replacementName)
+        {
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension == ".cs" || file.Extension == ".sln" || file.Extension == ".csproj" || file.Extension == ".config" || file.Extension == ".txt" || file.Extension == ".user" || file.Extension == ".cshtml" || file.Extension == ".asax")
+                {
+                    string text = File.ReadAllText(file.DirectoryName + "\\" + file.Name);
+                    text = text.Replace(currentName, replacementName);
+                    File.WriteAllText(file.DirectoryName + "\\" + file.Name, text, Encoding.UTF8);
+                }
+
+                if (file.Extension == ".cs" || file.Extension == ".sln" || file.Extension == ".csproj" || file.Extension == ".config" || file.Extension == ".txt" || file.Extension == ".user" || file.Extension == ".cshtml" || file.Extension == ".asax")
+                {
+                    File.Move(file.DirectoryName + "\\" + file.Name, file.DirectoryName + "\\" + file.Name.Replace(currentName, replacementName));
+                }
+            }
+        }
+
+        public void DeleteDirectory(string targetDir)
+        {
+            string[] files = Directory.GetFiles(targetDir);
+            string[] dirs = Directory.GetDirectories(targetDir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(targetDir, false);
+        }
+    }
+}
